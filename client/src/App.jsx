c27211255c8e5bd032c652c858dae4412a6ef875
Markswell <osoromarkswell80@gmail.com>
2024-01-20 21:41:53 +0300

    Populate Main.jsx & App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Switch } from "react-router";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Restaurant from "./components/Restaurant";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Navbar />
+    <Switch>
+      <Route exact path="/restaurants/:id">
+        <Restaurant />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </> 
   )
 }
 
